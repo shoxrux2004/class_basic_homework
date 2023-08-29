3bb47a0304d391d20844856f48bb99f646fe2c50
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-29 05:45:37 +0500

    3 object list

--- create_some_objects.py
+++ create_some_objects.py
@@ -1,4 +1,10 @@
 from create_one_attribute import Person
-
+class Person:
+    def __init__(self,name1):
+        self.name1=name1
+p1=Person("Anvar")
+p2=Person("Shavkat")
+print(p1.name1)
+print(p2.name1)
 #create an object named "p1" whose name is "Anvar"
 #create an object named "p2" whose name is "Shavkat"
